1b885d8989a33c9d05deabc0f0380988dc971349
Aurio Rajaa <riorajaa2018@gmail.com>
2024-01-08 15:58:01 +0700

    Ubah Login

--- login.php
+++ login.php
@@ -1,33 +1,33 @@
 <?php
-	@ob_start();
-	session_start();
-	if(isset($_POST['proses'])){
-		require 'config.php';
-			
-		$user = strip_tags($_POST['user']);
-		$pass = strip_tags($_POST['pass']);
+@ob_start();
+session_start();
+if (isset($_POST['proses'])) {
+    require 'config.php';
 
-		$sql = 'select member.*, login.user, login.pass
+    $user = strip_tags($_POST['user']);
+    $pass = strip_tags($_POST['pass']);
+
+    $sql = 'select member.*, login.user, login.pass
 				from member inner join login on member.id_member = login.id_member
 				where user =? and pass = md5(?)';
-		$row = $config->prepare($sql);
-		$row -> execute(array($user,$pass));
-		$jum = $row -> rowCount();
-		if($jum > 0){
-			$hasil = $row -> fetch();
-			$_SESSION['admin'] = $hasil;
-			echo '<script>alert("Login Sukses");window.location="index.php"</script>';
-		}else{
-			echo '<script>alert("Login Gagal");history.go(-1);</script>';
-		}
-	}
+    $row = $config->prepare($sql);
+    $row->execute(array($user, $pass));
+    $jum = $row->rowCount();
+    if ($jum > 0) {
+        $hasil = $row->fetch();
+        $_SESSION['admin'] = $hasil;
+        echo '<script>alert("Login Sukses");window.location="index.php"</script>';
+    } else {
+        echo '<script>alert("Login Gagal");history.go(-1);</script>';
+    }
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-<meta charset="utf-8">
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
@@ -50,7 +50,7 @@
             </form>
         </div>
         <div class="form-container sign-in">
-		<form action="login.php" method="POST">
+            <form action="login.php" method="POST">
                 <h1>Sign In</h1>
                 <input type="text" placeholder="username" name="user">
                 <input type="password" placeholder="Password" name="pass">
@@ -60,7 +60,7 @@
         <div class="toggle-container">
             <div class="toggle">
                 <div class="toggle-panel toggle-right">
-                    <h1>Hello, Admin!</h1>
+                    <h1>Hai, Admin!</h1>
                     <p>Have a Nice day ! </p>
                 </div>
             </div>
@@ -68,13 +68,13 @@
     </div>
 
     <script>
-		const container = document.getElementById('container');
-		const loginBtn = document.getElementById('login');
+        const container = document.getElementById('container');
+        const loginBtn = document.getElementById('login');
 
-		loginBtn.addEventListener('click', () => {
-			container.classList.remove("active");
-		});
-	</script>
+        loginBtn.addEventListener('click', () => {
+            container.classList.remove("active");
+        });
+    </script>
     <!-- Core plugin JavaScript-->
     <script src="sb-admin/vendor/jquery-easing/jquery.easing.min.js"></script>
     <!-- Custom scripts for all pages-->
